77d38858d7eaa62d1885e89f5fc4e05c479f5962
yuzushioh <yuzushioh@gmail.com>
2018-06-12 14:36:40 +0900

    refactor cache class, add methods to cache and load object confirmed to Codable

--- Wei/Utility/Cache.swift
+++ Wei/Utility/Cache.swift
@@ -10,9 +10,13 @@ import PINCache
 import RxSwift
 import RxCocoa
 
+enum CacheKey: String {
+    case transactionHistory
+}
+
 protocol CacheProtocol {
-    func save<Request: WeiRequest>(_ object: NSCoding, for request: Request)
-    func load<Request: WeiRequest>(for request: Request) -> Maybe<Request.Response> where Request.Response: Decodable
+    func save(_ object: NSCoding, for key: String)
+    func load(for key: String) -> Maybe<Any>
     func clear()
 }
 
@@ -22,40 +26,64 @@ final class Cache: CacheProtocol {
     
     init() {}
     
-    func save<Request>(_ object: NSCoding, for request: Request) where Request: WeiRequest {
+    func save(_ object: NSCoding, for key: String) {
         DispatchQueue.global().async { [weak backend] in
-            backend?.setObject(object, forKey: request.cacheKey)
+            backend?.setObject(object, forKey: key)
         }
     }
     
-    func load<Request>(for request: Request) -> Maybe<Request.Response> where Request: WeiRequest, Request.Response: Decodable {
-        return Observable<Request.Response>
+    func load(for key: String) -> Maybe<Any> {
+        return Observable<Any>
             .create { [weak backend] observer in
-                backend?.object(forKey: request.cacheKey) { caching, key, object in
+                backend?.object(forKey: key) { caching, key, object in
                     guard let object = object else {
                         observer.onCompleted()
                         return
                     }
-                    
-                    let response: Request.Response
-                    do {
-                        response = try request.response(from: object)
-                    } catch let error {
-                        observer.onError(error)
-                        observer.onCompleted()
-                        return
-                    }
-                    
-                    observer.onNext(response)
+                    observer.onNext(object)
                     observer.onCompleted()
                 }
-                
                 return Disposables.create()
             }
+            .observeOn(DriverSharingStrategy.scheduler)
             .asMaybe()
     }
     
     func clear() {
         backend.removeAllObjects()
+    }
+}
+
+extension CacheProtocol {
+    func save<Request: WeiRequest>(_ object: NSCoding, for request: Request) {
+        save(object, for: request.cacheKey)
+    }
+    
+    func save<Object: Encodable>(_ object: Object, for cacheKey: CacheKey) {
+        guard let data = try? JSONEncoder().encode(object) else {
+            return
+        }
+        save(data as NSCoding, for: cacheKey.rawValue)
+    }
+    
+    func load<Request: WeiRequest>(for request: Request) -> Maybe<Request.Response> where Request.Response: Decodable {
+        return load(for: request.cacheKey).flatMap { object -> Maybe<Request.Response> in
+            let response = try? request.response(from: object)
+            if let response = response {
+                return Maybe.just(response)
+            }
+            return Maybe.empty()
+        }
+    }
+    
+    func load<Object: Decodable>(type: Object.Type = Object.self, for cacheKey: CacheKey) -> Maybe<Object> {
+        return load(for: cacheKey.rawValue).flatMap { object -> Maybe<Object> in
+            let data = object as? Data
+            let decoded = data.flatMap { try? JSONDecoder().decode(type, from: $0) }
+            if let decoded = decoded {
+                return Maybe.just(decoded)
+            }
+            return Maybe.empty()
+        }
     }
 }
