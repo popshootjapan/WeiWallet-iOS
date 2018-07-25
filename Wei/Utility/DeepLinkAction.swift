2e3db270a08fa4feaaf98acb3dd1d90addac051e
yuzushioh <yuzushioh@gmail.com>
2018-07-25 15:04:36 +0900

    add broadcast transaction to deeplink action

--- Wei/Utility/DeepLinkAction.swift
+++ Wei/Utility/DeepLinkAction.swift
@@ -12,6 +12,7 @@ import EthereumKit
 enum DeepLinkAction {
     case signMessage(message: String, callbackScheme: String)
     case signTransaction(rawTransaction: RawTransaction, callbackScheme: String)
+    case broadcastTransaction(rawTransaction: RawTransaction, callbackScheme: String)
     
     init?(url: URL) throws {
         guard let urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false),
@@ -19,22 +20,32 @@ enum DeepLinkAction {
                 return nil
         }
         
+        guard let callBackScheme = urlComponents.queryItems?.first(where: { $0.name == "callback_scheme" })?.value else {
+            return nil
+        }
+        
         switch (host, urlComponents.path) {
         case ("sdk", "/personal_sign"):
-            guard let message = urlComponents.queryItems?.first(where: { $0.name == "message"})?.value,
-                let callBackScheme = urlComponents.queryItems?.first(where: { $0.name == "callback_scheme" })?.value else {
+            guard let message = urlComponents.queryItems?.first(where: { $0.name == "message"})?.value else {
                 return nil
             }
             self = .signMessage(message: message, callbackScheme: callBackScheme)
             
         case ("sdk", "/sign_transaction"):
-            guard let hex = urlComponents.queryItems?.first(where: { $0.name == "raw_transaction" })?.value,
-                let callBackScheme = urlComponents.queryItems?.first(where: { $0.name == "callback_scheme" })?.value else {
+            guard let hex = urlComponents.queryItems?.first(where: { $0.name == "raw_transaction" })?.value else {
                 return nil
             }
             
             let rawTransaction = try JSONDecoder().decode(RawTransaction.self, from: Data(hex: hex))
             self = .signTransaction(rawTransaction: rawTransaction, callbackScheme: callBackScheme)
+            
+        case ("sdk", "/broadcast_transaction"):
+            guard let hex = urlComponents.queryItems?.first(where: { $0.name == "raw_transaction" })?.value else {
+                    return nil
+            }
+            
+            let rawTransaction = try JSONDecoder().decode(RawTransaction.self, from: Data(hex: hex))
+            self = .broadcastTransaction(rawTransaction: rawTransaction, callbackScheme: callBackScheme)
             
         default:
             return nil
