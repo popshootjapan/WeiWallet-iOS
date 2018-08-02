5c7945e7137db225e25c7f6e523c0259e1c1082b
yuzushioh <yuzushioh@gmail.com>
2018-08-02 16:41:24 +0900

    connect IBOutlet

--- Wei/View/SignTransaction/SignTransactionViewController.swift
+++ Wei/View/SignTransaction/SignTransactionViewController.swift
@@ -13,6 +13,18 @@ final class SignTransactionViewController: UIViewController {
     var completionHandler: ((String) -> Void)!
     var viewModel: SignTransactionViewModel!
     
+    
+    
+    @IBOutlet private var fiatAmountLabels: [UILabel]!
+    @IBOutlet private var fiatFeeLabels: [UILabel]!
+    @IBOutlet private var fiatCurrencyLabels: [UILabel]!
+    @IBOutlet private weak var etherAmountLabel: UILabel!
+    @IBOutlet private weak var toAddressLabel: UILabel!
+    @IBOutlet private weak var fiatEtherLabel: UILabel!
+    @IBOutlet private weak var totalFiatAmountLabel: UILabel!
+    @IBOutlet private weak var cancelButton: UIBarButtonItem!
+    @IBOutlet private weak var doneButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
